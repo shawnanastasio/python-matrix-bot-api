29431ee945183f64b529dcc2e85e374fabf53635
Shawn Anastasio <shawnanastasio@yahoo.com>
2017-05-10 22:00:52 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+DESC='A library for creating bots on the Matrix communication network.'
+
+setup(
+    name='matrix_bot_api',
+    version='0.1',
+    author='Shawn Anastasio',
+    author_email='see github',
+    url='https://github.com/shawnanastasio/python-matrix_bot_api',
+    packages=['matrix_bot_api',],
+    install_requires=['matrix_python_sdk'],
+    license='GNU GPL v3',
+    summary=DESC,
+    long_description=DESC,
+)
